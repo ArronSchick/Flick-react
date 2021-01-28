7045f0a0f42605c57c39402eb7667de99ec4731a
ArronSchick <arron.schick@gmail.com>
2021-01-28 15:22:31 +1100

    splash page nav,signin and signup form components +  side bar created with basic routes

---
+++ src/components/Splash.js
@@ -0,0 +1,25 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Link
+} from "react-router-dom";
+import './Splash.css';
+import Navbar from "./Navbar";
+
+
+const Splash = () => {
+    return (
+        <div>
+            <Navbar />
+            <div className="splashContainer">
+                <div className="splashBody">
+                    <h1 className="splashTitle">FLICK</h1>
+                    <Link to="/signup"><button  className="signUp btn" onClick={e => e.target} >SIGN UP</button></Link>
+                    <button className="splashRandom btn">RANDOM MOVIE</button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Splash
