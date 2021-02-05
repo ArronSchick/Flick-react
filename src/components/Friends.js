286e9f1bafd5e15992652e898688dc66a581815e
robyn <65057146+robynsdev@users.noreply.github.com>
2021-02-05 18:10:54 +1100

    cleanup of unused import in Friends.js

--- src/components/Friends.js
+++ src/components/Friends.js
@@ -1,49 +1,56 @@
-import React,{useState, useEffect, useContext} from "react";
-import {useGlobalState} from '../utils/stateContext'
-import {addFriends} from '../services/friendServices'
-import './styles/templateDashboard.css';
-import {Button, Label, Input} from './Styled'
-import ShowFriends from './ShowFriends'
+import React, { useState } from "react";
+import { addFriends } from "../services/friendServices";
+import "./styles/templateDashboard.css";
+import { Button, Input } from "./Styled";
+import ShowFriends from "./ShowFriends";
 
-export default function Friends (){
-    const initialFormState = {
-        email: ''
-    }
+export default function Friends() {
+  const initialFormState = {
+    email: "",
+  };
 
-    const [formState, setFormState] = useState(initialFormState)
-    const {store} = useGlobalState()
-    const {auth} = store
+  const [formState, setFormState] = useState(initialFormState);
 
-    function handleSubmit(event) {
-        event.preventDefault()
-        addFriends(formState)
-        .then(({email}) => {
-            console.log(email);
-        })
-        .catch((error) => console.log(error))
-    }
+  function handleSubmit(event) {
+    event.preventDefault();
+    addFriends(formState)
+      .then(({ email }) => {
+        console.log(email);
+      })
+      .catch((error) => console.log(error));
+  }
 
-    function handleChange(event) {
-        setFormState({
-            ...formState,
-            [event.target.name]: event.target.value
-        })
-    }
+  function handleChange(event) {
+    setFormState({
+      ...formState,
+      [event.target.name]: event.target.value,
+    });
+  }
 
-    return (
-        <div className="dtContainer">
-            <div className="dtMain">
-                <h1 className="dtTitle">FRIENDS</h1>
-                <div className="dtCard">
-                    <div className="dtSearch">
-                        <Input placeholder="email" className = "email placeColor" id="email" type='email' name='email' value={formState.email} onChange={handleChange}></Input>
-                        <Button onClick={handleSubmit} value="Add Friend">Add Friend</Button>
-                    </div>
-                    <div className="dtListContainer">
-                        <ShowFriends />
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="dtContainer">
+      <div className="dtMain">
+        <h1 className="dtTitle">FRIENDS</h1>
+        <div className="dtCard">
+          <div className="dtSearch">
+            <Input
+              placeholder="email"
+              className="email placeColor"
+              id="email"
+              type="email"
+              name="email"
+              value={formState.email}
+              onChange={handleChange}
+            ></Input>
+            <Button onClick={handleSubmit} value="Add Friend">
+              Add Friend
+            </Button>
+          </div>
+          <div className="dtListContainer">
+            <ShowFriends />
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
